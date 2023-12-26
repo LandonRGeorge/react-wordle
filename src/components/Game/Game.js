f7c47aabaf5211943395723120fad7057eaa64a6
LandonRGeorge <landon.robert.george@gmail.com>
2023-12-26 12:26:45 -0600

    finish

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,77 @@
 import React from 'react';
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import {sample} from '../../utils';
+import {WORDS} from '../../data';
+import Guess from '../Guess';
+import {NUM_OF_GUESSES_ALLOWED} from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.info({answer});
+
+function Happy({nbrGuesses}) {
+  return (
+    <div className="happy banner">
+      <p>
+        <strong>Congratulations!</strong> Got it in
+        {' '}
+        <strong>{nbrGuesses} guesses</strong>.
+      </p>
+    </div>
+  )
+}
+
+function Sad({answer}) {
+  return (
+    <div className="sad banner">
+      <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
+    </div>
+  )
+}
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState("")
+  const [guesses, setGuesses] = React.useState([])
+  const [won, setWon] = React.useState()
+
+  const formHandler = e => {
+    e.preventDefault();
+
+    const newGuess = {
+      id: Math.random(),
+      value: guess,
+    }
+    setGuesses([...guesses, newGuess])
+    setGuess("");
+  }
+
+  if (won === undefined && guesses.length >= NUM_OF_GUESSES_ALLOWED) {
+    setWon(false)
+  }
+
+  const display = () => {
+    if (won === true) return <Happy nbrGuesses={guesses.length}/>
+    if (won === false) return <Sad answer={answer}/>
+    return
+  }
+  return (
+    <>
+      <Guess guesses={guesses} answer={answer} hasWon={(resp) => setWon(resp)}/>
+      <form className="guess-input-wrapper"
+            onSubmit={formHandler}
+      >
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input id="guess-input" type="text"
+               value={guess}
+               minLength="5"
+               maxLength="5"
+               onChange={e => setGuess(e.target.value.toUpperCase())}
+        />
+      </form>
+      {display()}
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {NUM_OF_GUESSES_ALLOWED} from "../../constants";
+import {checkGuess} from "../../game-helpers";
+
+function Row({guess, answer, hasWon}) {
+  console.log(answer)
+  if (guess===undefined) {
+    return (
+      <p className="guess">
+        {Array(5).fill(0).map((_, j) => j).map(j => (
+          <span className="cell" key={j}></span>
+        ))}
+      </p>
+    )
+  }
+
+  if (guess.value === answer) {
+    hasWon(true)
+  }
+
+  const checked = checkGuess(guess.value, answer)
+  return (
+    <p className="guess">
+      {checked.map((v, i)=> (
+        <span className={`cell ${v.status}`} key={i}>{v.letter}</span>
+      ))}
+    </p>
+  )
+}
+
+function Guess({guesses, answer, hasWon}) {
+  return (
+    <div className="guess-results">
+      {Array(NUM_OF_GUESSES_ALLOWED).fill(0).map((_, i) => i).map(i => {
+        let guess = guesses[i];
+        return (
+          <Row guess={guess} key={i} answer={answer} hasWon={hasWon}/>
+          )
+        })}
+      </div>
+  )
+}
+
+export default Guess;
